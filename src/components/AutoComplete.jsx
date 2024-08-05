c66e0ee81e3e7c5c6c9815d0af77237fe3d49ac5
Pavo1034 <kumarpawan963258@gmail.com>
2024-08-05 23:19:57 +0530

    made responsive

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,21 +1,17 @@
-import React, {useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import SuggestionsList from "./Suggestions-list";
 import { useDispatch, useSelector } from "react-redux";
-import { addData, clearData } from "../utils/dataSlice";
+import { addData, clearData, removeItem } from "../utils/dataSlice";
+import moment from "moment";
 
-
-const AutoComplete = ({
-  placeholder,
-  fetchSuggestions,
-}) => {
+const AutoComplete = ({ placeholder, fetchSuggestions }) => {
   const [inputVal, setInputVal] = useState("");
   const [Suggestions, setSuggestions] = useState([]);
   const [loading, setLoading] = useState();
   const [error, setError] = useState(null);
-  const dispatch= useDispatch();
-  const history =useSelector(store=>store.searchdata?.data);
+  const dispatch = useDispatch();
+  const history = useSelector((store) => store.searchdata?.data);
   console.log(history);
-  // console.log(Suggestions);
   const HandleInput = (event) => {
     setInputVal(event.target.value);
   };
@@ -32,30 +28,39 @@ const AutoComplete = ({
       setLoading(false);
     }
   };
-   
- const clearHistory =()=>{
-  dispatch(clearData());
- }
+
+  const clearHistory = () => {
+    dispatch(clearData());
+  };
   useEffect(() => {
     if (inputVal?.length > 1) {
       getSuggestions(inputVal);
     }
   }, [inputVal]);
-  const handleSuggestioClick=(suggestion)=>{
-    setInputVal(suggestion)
-    setSuggestions([])
-    dispatch(addData(suggestion))
-  }
+  const handleSuggestioClick = (suggestion) => {
+    setInputVal(suggestion);
+    setSuggestions([]);
+    let date = new Date();
+    let currentDate = moment(date).format("DD/MM/YYYY");
+    let obj = {
+      title: suggestion,
+      date: currentDate,
+    };
+    dispatch(addData(obj));
+  };
+  const removelist = (index) => {
+    dispatch(removeItem(index));
+  };
   return (
     <div className="w-full">
-    <div className="w-full flex justify-center items-center  mt-10 h-16">
-      <input
-        type="text"
-        placeholder={placeholder}
-        value={inputVal}
-        onChange={HandleInput}
-        className="w-[50%] h-[70%] outline-none pl-5  rounded-md border-2 border-black"
-      />
+      <div className="w-full flex justify-center items-center px-5 mt-10 h-16">
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={inputVal}
+          onChange={HandleInput}
+          className=" w-full md:w-[50%] md:h-[70%] outline-none pl-5  rounded-md border-2 border-black"
+        />
       </div>
       {(Suggestions.length > 0 || loading || error) && (
         <>
@@ -66,26 +71,39 @@ const AutoComplete = ({
               <div className="loading animate-spin border-2 border-red-600 border-b-0 border-l-0 w-14 h-14 rounded-full"></div>
             )}
           </div>
-          <div className="w-[40%]  mx-auto bg-white rounded-md shadow">
-            <SuggestionsList 
-            suggestions={Suggestions}
-            onSuggestionClick={handleSuggestioClick} />
+          <div className="md:w-[48%] mx-5  md:mx-auto bg-white  shadow-2xl">
+            <SuggestionsList
+              suggestions={Suggestions}
+              onSuggestionClick={handleSuggestioClick}
+            />
           </div>
-          <div className="w-[40%] mt-6 p-4  mx-auto bg-slate-50 shadow-lg rounded-md ">
-         <div className="flex justify-between gap-10 mb-10">
-         <div className="text-bold text-xl ">Search History</div>
-         <button onClick={clearHistory} className="bg-slate-400 p-2 rounded-sm">clear history</button>
-         </div>
-            {
-              history.map((data,index)=><div key={index} className="px-3 hover:bg-slate-200/40 cursor-pointer list-none  space-y-2 ">
-                <span>{data}</span>
-              </div>)
-            }
+          <div className="md:w-[48%] mt-6 p-4 mx-5 md:mx-auto bg-white shadow-2xl rounded-md ">
+            <div className="flex justify-between gap-10 mb-10">
+              <div className="text-bold text-xl ">Search History</div>
+              <button
+                onClick={clearHistory}
+                className="bg-slate-400 p-2 rounded-sm"
+              >
+                clear history
+              </button>
+            </div>
+
+            {history.map((data, index) => (
+              <div
+                key={index}
+                className="px-3 hover:bg-slate-200/40 cursor-pointer list-none  flex justify-between items-center space-y-2 "
+              >
+                <span>{data.title}</span>
+               <div className="flex justify-between items-center gap-5">
+               <span>{data.date}</span>
+               <button onClick={()=>{removelist(index)}}>❌</button>
+               </div>
+              </div>
+            ))}
           </div>
         </>
       )}
-      </div>
-    
+    </div>
   );
 };
 
--- src/utils/dataSlice.js
+++ src/utils/dataSlice.js
@@ -3,17 +3,20 @@ import { createSlice } from "@reduxjs/toolkit"
 const dataSlice = createSlice({
     name:"searchdata",
     initialState:{
-        data:[]
+        data:[],
     },
     reducers:{
       addData:(state,action)=>{
         state.data.push(action.payload)
       },
+      removeItem:(state,action)=>{
+         state.data.splice(action.payload,1)
+      },
       clearData:(state,action)=>{
         state.data.length=0;
-      }
+      },
     }
 })
 
-export const {addData,clearData}=dataSlice.actions;
+export const {addData,clearData,removeItem}=dataSlice.actions;
 export default dataSlice.reducer;
